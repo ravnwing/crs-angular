89490a8d14a5ffa5c02361ed2fb6347d256b825e
Greg Walker <ravnwing@gmail.com>
2018-05-09 16:05:02 -0400

    Week 1 Working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import 'hammerjs';
+import {MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
+  MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule, MatSlideToggleModule,
+  MatToolbarModule, MatListModule, MatGridListModule, MatCardModule, MatIconModule,
+  MatProgressSpinnerModule, MatDialogModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
+    MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule, MatSlideToggleModule,
+    MatToolbarModule, MatListModule, MatGridListModule, MatCardModule, MatIconModule,
+    MatProgressSpinnerModule, MatDialogModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
