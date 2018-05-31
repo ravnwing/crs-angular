e6eed4a3019599f3d116a66374c5403f311497c2
Greg Walker <ravnwing@gmail.com>
2018-05-31 13:52:55 -0400

    routes, working, unclean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,19 @@ import {MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldMod
   MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule, MatSlideToggleModule,
   MatToolbarModule, MatListModule, MatGridListModule, MatCardModule, MatIconModule,
   MatProgressSpinnerModule, MatDialogModule} from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -22,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +41,12 @@ import { FooterComponent } from './footer/footer.component';
     MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule, MatSlideToggleModule,
     MatToolbarModule, MatListModule, MatGridListModule, MatCardModule, MatIconModule,
     MatProgressSpinnerModule, MatDialogModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    RouterModule.forRoot([
+      { path: 'home',  component: HomeComponent },
+      { path: 'menu',     component: MenuComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' }
+    ])
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
